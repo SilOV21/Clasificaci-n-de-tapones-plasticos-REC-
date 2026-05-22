b2d2f5b6f51d56c8b4dcb38f98836ed85d1a7b10
rutalva29 <bastidasalvaruthalejandra@gmail.com>
2026-05-22 23:37:25 +0200

    Delete ur3_vision_control directory

--- ur3_vision_control/build/ur3_vision_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/delia/Documents/MUAR/ProyectoREC/ur3_vision_control/install/ur3_vision_control'
